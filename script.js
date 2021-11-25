1c2110ba870f0f679609c36e8a3576148776728c
uno0040 <montanhauno0040@gmail.com>
2021-11-24 23:48:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var resposta = '';
 var resultado;
 function round1() {
     var pergunta = Math.floor(Math.random() * 11);
-    // funcao de aleatorio bla bla bla
+    // funcao de aleatorio bla bla bla bla
     if (resposta = perguntas[pergunta][1]) {
         resultado = 'passou'
     }
