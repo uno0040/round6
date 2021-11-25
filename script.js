8a10880c8fd6db5ba6b11641f790f6bf87d4caef
uno0040 <montanhauno0040@gmail.com>
2021-11-25 11:45:27 -0300

    Montando logica
    Esse jeito que estou fazendo nao eh o melhor, se alguem tiver uma ideia logica melhor avisa

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 var perguntas = [['exemplo','3']]
 var resposta = '';
-var resultado;
-function round1() {
+var jogoContinua; // <- boolean que define se o jogador morreu ou nao no jogo.  true -> vivo      false -> morto
+/*do jeito que eu estou montando, vai ter um array com as perguntas e respostas, e uma funcao para cada "coisa"
+se tiver uma ideia melhor de como fazer isso estou ouvindo, pois a maneira que estou fazendo me parece relativamente preguicosa*/
+function round1() { 
     var pergunta = Math.floor(Math.random() * 11);
-    // funcao de aleatorio bla bla bla bla
     if (resposta = perguntas[pergunta][1]) {
-        resultado = 'passou'
+        jogoContinua = true
+        checkVivo()
     }
     else {
-        resultado = 'morreu'
+        jogoContinua = false
+        checkVivo()
     }
+}
+function checkVivo() { // <- funcao que vai checar se o jogador esta vivo ou nao e mudar o jogo de acordo com o estado do jogador  
+    if (jogoContinua == true) {
+        // nao sei oq botar aqui ainda
+    }
+    else if (jogoContinua == false) {
+        fimDeJogo()
+    }
+}
+
+function fimDeJogo(){ //<- funcao que vai declarar o fim de jogo ao jogador e chamar a funcao de reinicio de jogo
+
+}
+
+function reiniciaJogo(){ // <- funcao que vai reiniciar o jogo
+
 }
